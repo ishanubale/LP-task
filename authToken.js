98aa0ca1310c29cb6fad4233eb99378a9ea7d385
ishanubale <76592703+ishanubale@users.noreply.github.com>
2024-11-22 10:31:38 +0530

    Update authToken.js

--- authToken.js
+++ authToken.js
@@ -7,7 +7,7 @@ catch(err){
 }
 botContext.printDebugMessage("EngagementId  " + engagementId + jwt);
 
-if(engagementId == 4957982238 || jwt){ 
+if(engagementId == 5174201838 || jwt){ 
 botContext.setTriggerNextMessage('multiple_choice_question_36');
 }
 else{
